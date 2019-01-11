8bd972a28e976e1107474e51aaae480935b637a7
Garrett <gjblythe@gmail.com>
2019-01-11 12:48:13 -0700

    routes

--- GitGoingApp/src/GitInit.js
+++ GitGoingApp/src/GitInit.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { TouchableOpacity, Text, Button, StyleSheet, View } from 'react-native'
-import { Actions, Scene } from 'react-native-router-flux'
+import { Actions } from 'react-native-router-flux'
 import AntiClippy from './AntiClippy';
 
 const GitInit = () => {
