710595806bac727622a63144abbd1206da4928d8
Heather Drattlo <hdrattlo88@yahoo.com>
2019-01-10 20:58:12 -0700

    all pages styled

---
+++ GitGoingApp/src/AntiClippy.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import { Image } from 'react-native'
+
+const AntiClippy = () => (
+   <Image source = {require('../assets/antiClippy.png')} />
+)
+export default AntiClippy
--- GitGoingApp/src/GitReset.js
+++ GitGoingApp/src/GitReset.js
@@ -1,20 +1,103 @@
 import React from 'react'
-import { TouchableOpacity, Text, Button } from 'react-native'
+import { TouchableOpacity, Text, Button, StyleSheet, View } from 'react-native'
 import { Actions } from 'react-native-router-flux'
+import AntiClippy from './AntiClippy'
 
 const GitReset = () => {
    const goToStash = () => {
       Actions.gitStash()
    }
    return (
-      <TouchableOpacity style = {{ margin: 128 }} >
-         <Text style= {{fontSize: 30}}>Git Reset</Text>
-         <Text>Oh shit button. Undoes all commits, preserving changes locally. Start over if needed.</Text>
+      <View style={styles.container}>
+      <View style={styles.brownBox}><Text style={styles.title}>Git Reset</Text>
+      <View style={styles.circle1}>
+       
+      <Text style={styles.p}>Oh shit button. Undoes all commits, preserving changes locally. Start over if needed. </Text>
+      <AntiClippy style={styles.AntiClippy}/>
+     
+      </View>
+      </View>
+      <TouchableOpacity style={styles.button}>
          <Button 
          onPress = {goToStash}
-         title= "Git Stash"
+         title= "GitGoing to Git Stash"
+         color='#FF6D70'
          />
       </TouchableOpacity>
+      </View>
+         
+         
    )
 }
+//style
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      backgroundColor: '#4A4843',
+      alignItems: "center",      
+    },
+    
+   title: {
+      color: '#FF6D70',
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    fontSize: 60,
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 10,
+    marginTop: 0,
+    paddingTop: 75,
+    
+   },
+   //white circle
+  circle1: {
+   backgroundColor: '#fff',
+   width: 400,
+   height: 400,
+   alignItems: 'center',
+   justifyContent: 'center',
+   borderRadius: 200,
+   borderColor: '#4A4843',
+   borderWidth: 15,
+   marginTop:60,
+   flexDirection: 'row',
+   alignItems: 'center',
+   
+ },
+ //brown box
+ brownBox: {
+   backgroundColor: '#A79B82',
+   width: 410,
+   height:575,
+   alignItems: 'center',
+   justifyContent: 'center',
+   borderRadius: 220,
+   marginTop: 50,
+   paddingBottom: 25,
+ },
+
+//logo
+ AntiClippy: {
+   flexDirection: 'row',
+   alignItems: 'center',
+ },
+
+ 
+ button: {
+    color:'#FF6D70',
+    marginTop: 20,
+ },
+ p:{
+    color: 'black',
+    width: 200,
+    flexDirection: 'row',
+    alignItems: 'center',
+    fontSize: 18,
+
+ },
+
+
+ });
 export default GitReset
